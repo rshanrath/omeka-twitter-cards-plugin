b5e842ed351e4d418ec1645e5bdc66f15bef64f7
Scott Hanrath <scotthanrath@gmail.com>
2014-10-07 13:08:01 -0500

    Add cards to exhibit pages.  Decode html entities before stripping tags to handle exhibit descriptions

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Twitter Cards Plugin for Omeka
 
-A plugin to include [Twitter Card](https://dev.twitter.com/cards) tags on Omeka pages.  
+A plugin to include [Twitter Card](https://dev.twitter.com/cards) tags on [Omeka](http://omeka.org/) pages.  
 
-Tags for Twitter Card summary cards are added to Collection and Item pages for records that include dc.title and dc.description metadata. For Items with a file that has a thumbnail, a thumbnail image URL is added.  For other pages, the site title and description are used.
+Tags for Twitter Card summary cards are added to Exhibit, Collection and Item pages for records that include dc.title and dc.description metadata. For Items with a file that has a thumbnail, a thumbnail image URL is added.  Item metadata takes precedent over Exhibit metadata if an Item is viewed from an Exhibit. For other pages, the site title and description are used.
 
 ## Configuration
 
--- TwitterCardsPlugin.php
+++ TwitterCardsPlugin.php
@@ -24,6 +24,16 @@ class TwitterCardsPlugin extends Omeka_Plugin_AbstractPlugin
     $description = '';
     $image_url = '';
 
+    // Is the curent record an exhibit?  Use its metadata.
+    try {
+      $exhibit = get_current_record('exhibit');
+      $title = metadata($exhibit, 'title', array('no_escape' => false));
+      $description = metadata($exhibit, 'description',array('no_escape' => false));
+    }
+    catch (Omeka_View_Exception $ove){
+      //  no collection, don't do anything
+    }
+
     // Is the curent record an item?  Use its metadata.
     try {
       $item = get_current_record('item');
@@ -61,9 +71,9 @@ class TwitterCardsPlugin extends Omeka_Plugin_AbstractPlugin
     if (strlen($title) > 0 && strlen($description) > 0){
       echo '<meta property="twitter:card" content="summary" />';
       echo '<meta property="twitter:site" content="'.get_option(TWITTER_CARDS_SITE_HANDLE_OPTION).'" />';
-      echo '<meta property="twitter:title" content="'.strip_formatting($title).'" />';
-      echo '<meta property="twitter:description" content="'.strip_formatting($description).'" />';
-      
+      echo '<meta property="twitter:title" content="'.strip_tags(html_entity_decode($title)).'" />';
+      echo '<meta property="twitter:description" content="'.strip_tags(html_entity_decode($description)).'" />';
+
       if (strlen($image_url) > 0){
         echo '<meta property="twitter:image:src" content="'.$image_url.'" />';
       }
